6feb9a8997c5d2489f20d490eec8612eda6a434d
RVTicianelli <r.v.ticianelli@gmail.com>
2018-10-03 20:21:22 -0300

    Carrega campanhas na pagina inicial

--- src/pages/init/init.ts
+++ src/pages/init/init.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CampanhaService } from '../../services/domain/campanha.service';
+import { CampanhaDTO } from '../../models/campanha';
 
 @IonicPage()
 @Component({
@@ -9,12 +10,14 @@ import { CampanhaService } from '../../services/domain/campanha.service';
 })
 export class InitPage {
 
+  campanhas: CampanhaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public campanhaService: CampanhaService) {
   }
 
   ionViewDidLoad() {
     this.campanhaService.findAll().subscribe(response => {
-      console.log(response);
+      this.campanhas = response;
     },
     error => {
       console.log(error);
