81dd1eed94463470d96e8274e6a15e5829c66299
RVTicianelli <r.v.ticianelli@gmail.com>
2018-10-15 18:09:31 -0300

    Criado metodos para autenticacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -10,12 +12,24 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  creds : CredenciaisDTO = {
+    email: "",
+    senha: ""
+  }
+
+  constructor(public navCtrl: NavController, public menu: MenuController, public auth: AuthService) {
 
   }
 
   entrarSemCadastro() {
-    this.navCtrl.setRoot('InitPage');
+    console.log(this.creds);
+  }
+
+  login() {
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        console.log(response.headers.get('Authorization'));
+      })
   }
   
   ionViewWillEnter(){
